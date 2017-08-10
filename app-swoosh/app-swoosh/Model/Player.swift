a888bab7fa1aefc597e77e7f608dd1369c760a7c
Phuc Le Dien <phucledien@gmail.com>
2017-08-10 23:49:54 +0700

    Added player model

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Phuc Le Dien on 8/10/17.
+//  Copyright © 2017 Phuc Le Dien. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+    
+}
